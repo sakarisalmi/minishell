133995c01519b6ad902154bd7ba3450453615386
Sakari Salmi <sakarisalmi@hotmail.com>
2023-05-18 12:53:21 +0300

    minishell.h tidying up

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ssalmi <ssalmi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 10:01:20 by ssalmi            #+#    #+#             */
-/*   Updated: 2023/05/17 17:10:32 by ssalmi           ###   ########.fr       */
+/*   Updated: 2023/05/18 12:52:29 by ssalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,40 +79,40 @@ void	turnon_echo(struct termios *termios);
 void	signal_handler(int signum);
 
 // pwd.c
-int			pwd(void);
+int		pwd(void);
 
 // env.c
-int			env(t_data *data);
+int		env(t_data *data);
 
 // check_for_builtin.c
-int			check_for_builtin(t_token **tokens_array);
+int		check_for_builtin(t_token **tokens_array);
 
 // ft_funcs.c
-int			ft_isspace(char c);
-char		*ft_strcpy(char *dest, const char *src);
-char		*ft_strncpy(char *dest, const char *src, size_t n);
-char		*ft_strncat(char *dest, const char *src, size_t n);
-void		*ft_realloc(void *ptr, size_t size);
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
-int			ft_strcmp(const char *s1, const char *s2);
+int		ft_isspace(char c);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strncat(char *dest, const char *src, size_t n);
+void	*ft_realloc(void *ptr, size_t size);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 
 // ft_strncmp_casein.c
-int			ft_strncmp_casein(const char *s1, const char *s2, size_t n);
+int		ft_strncmp_casein(const char *s1, const char *s2, size_t n);
 
 // str_array_add_str.c
-char		**str_array_add_str(char **str_array, char *new_str);
-char		**str_array_remove_str_by_index(char **str_array, int index);
-void		str_array_free_everything(char **str_array);
+char	**str_array_add_str(char **str_array, char *new_str);
+char	**str_array_remove_str_by_index(char **str_array, int index);
+void	str_array_free_everything(char **str_array);
 
 // minishell_parser.c
-int			minishell_parser(char *read_line, t_data *data);
-void		rl_replace_line(const char *text, int clear_undo);
+int		minishell_parser(char *read_line, t_data *data);
+void	rl_replace_line(const char *text, int clear_undo);
 
 // executor.c
-int			test_executor_pre_setup(t_data *data);
-int			real_executor_pre_setup(t_data *data);
+int		test_executor_pre_setup(t_data *data);
+int		real_executor_pre_setup(t_data *data);
 
 // minishell_loop_clean_up.c
-void		minishell_loop_clean_up(t_data *data);
+void	minishell_loop_clean_up(t_data *data);
 
 #endif
